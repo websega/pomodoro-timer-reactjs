1d0c5359ee13de6fb55ae6e3bac7d9e2b528af23
Sergey Vakhramov <vsa8813@gmail.com>
2021-01-02 20:10:58 +0500

    Delete point

--- src/components/TimeBar/TimeBar.js
+++ src/components/TimeBar/TimeBar.js
@@ -5,34 +5,25 @@ import classes from './TimeBar.scss';
 
 const TimeBar = ({ radius, circumference, dashOffset }) => {
   return (
-    <>
-      <svg className={classes.TimeBar}>
-        <circle
-          r={radius}
-          cx="185"
-          cy="185"
-          fill="none"
-          strokeDasharray={circumference}
-        />
-        <circle
-          r={radius}
-          cx="185"
-          cy="185"
-          fill="none"
-          strokeDasharray={circumference}
-          strokeDashoffset={dashOffset}
-          strokeLinecap="round"
-          transform="rotate(-90,185,185)"
-        />
-      </svg>
-      <svg
-        className={classes.Point}
+    <svg className={classes.TimeBar}>
+      <circle
+        r={radius}
+        cx="185"
+        cy="185"
         fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <circle cx="15" cy="15" r="15" />
-      </svg>
-    </>
+        strokeDasharray={circumference}
+      />
+      <circle
+        r={radius}
+        cx="185"
+        cy="185"
+        fill="none"
+        strokeDasharray={circumference}
+        strokeDashoffset={dashOffset}
+        strokeLinecap="round"
+        transform="rotate(-90,185,185)"
+      />
+    </svg>
   );
 };
 
